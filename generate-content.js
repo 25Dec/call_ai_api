4c06a6e96cc86f7b4a8650ee0e85e3319723ce29
25Dec <thiennhannguyenhuynh@gmail.com>
2024-07-03 15:13:11 +0700

    20240703

--- generate-content.js
+++ generate-content.js
@@ -1,20 +1,4 @@
 import { Client, logger } from 'camunda-external-task-client-js';
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
-const dotenv = require('dotenv').config();
-const { GoogleGenerativeAI } = require('@google/generative-ai');
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
-
-const gemini = async (prompt) => {
-	console.log('hi');
-	const result = await model.generateContent(prompt);
-	const response = await result.response;
-	const text = response.text();
-
-	console.log(text);
-};
 
 // configuration for the Client:
 //  - 'baseUrl': url to the Process Engine
@@ -28,7 +12,7 @@ const config = {
 const client = new Client(config);
 
 client.subscribe('GenerateConTent', async function ({ task, taskService }) {
-	await gemini();
+	await gemini('Tạo danh sách từ khóa theo chủ đề thú cưng');
 	// complete the task
 	try {
 		await taskService.complete(task);
